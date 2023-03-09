2ccfeb7ee811e1473f310e467a8a57d7fef541cd
shohann <shohanurr490@gmail.com>
2023-03-09 16:45:43 +0600

    db url fixed

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log('Hello');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const port = 3001;
+const app = (0, express_1.default)();
+app.listen(port, () => {
+    console.log(`Server started on port: ${port}`);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-console.log('Hello');
+import express, { NextFunction, Request, Response } from 'express';
+
+const port: number  = 3001;
+const app = express();
+
+
+app.listen(port, () => {
+    console.log(`Server started on port: ${port}`);
+});
