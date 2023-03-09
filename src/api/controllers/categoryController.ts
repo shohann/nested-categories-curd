f9646dabc9c786e27f557f3758ad009b4cafc60b
shohann <shohanurr490@gmail.com>
2023-03-10 02:06:32 +0600

    db url fixed

--- src/api/controllers/categoryController.ts
+++ src/api/controllers/categoryController.ts
@@ -1,10 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
-import { createCategory } from '../services/categoryService';
+import { createCategory,
+         fetchCategories,
+         fetchCategory,
+         fetchCategoryParent
+       } from '../services/categoryService';
 
 export const getCategories = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        
-        res.send('Works')
+        const categories = await fetchCategories();
+        res.send(categories)
     } catch (error) {
         console.log(error);
         res.send(error)
@@ -13,12 +17,41 @@ export const getCategories = async (req: Request, res: Response, next: NextFunct
 
 export const setCategory = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const { name } = req.body;
-
-        const category = await createCategory(name);
+        const { name, parentCategory } = req.body;
+        const category = await createCategory(name, parentCategory);
         
         res.send(category);
     } catch (error) {
+        // duplicate "code": "P2002",
+        console.log(error);
+        res.send(error)
+    }
+};
+
+export const getCategory = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { categoryId } = req.params;
+        const category = await fetchCategory(categoryId);
+        let parent;
+
+        if (category && category.parentCategory !== 'root') {
+            parent = await fetchCategoryParent(category.parentCategory);
+            return res.send({
+                category,
+                parent
+            })
+        } else {
+            // not found error
+        }
+
+        
+        
+        res.send({
+            category,
+            // parentCategory
+        });
+    } catch (error) {
+        // not found
         console.log(error);
         res.send(error)
     }
--- src/api/routes/categoryRoutes.ts
+++ src/api/routes/categoryRoutes.ts
@@ -1,10 +1,16 @@
 import express from 'express';
-import { getCategories, setCategory } from '../controllers/categoryController';
+import { getCategories, 
+         setCategory, 
+         getCategory 
+       } from '../controllers/categoryController';
 
 const router = express.Router();
 
 router.route('/')
       .get(getCategories)
       .post(setCategory)
+
+router.route('/:categoryId')
+      .get(getCategory)
 
 export default router;
